2a1b26fc92a9688e8b5a5149936aa778f7f6992b
Pawel Grudzien <erzetpe@gmail.com>
2020-10-19 19:41:42 +0200

    Add AWS API support and some simple checks

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/epiphany-platform/aws-basic-infrastructure
 go 1.15
 
 require (
+	github.com/aws/aws-sdk-go v1.15.77
 	github.com/go-test/deep v1.0.7
 	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0
 )
--- tests/CreateInfrastructureIT_test.go
+++ tests/CreateInfrastructureIT_test.go
@@ -9,19 +9,25 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/service/resourcegroups"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/epiphany-platform/aws-basic-infrastructure/tests/utils"
 )
 
 const (
 	imageTag   = "epiphanyplatform/awsbi:0.0.1"
+	awsTag     = "awsbi-module"
 	moduleName = "awsbi-module"
 	sshKeyName = "vms_rsa"
 	path       = "./"
 )
 
 var (
-	awsAccessKey              string = "M_AWS_ACCESS_KEY=" + os.Getenv("AWS_ACCESS_KEY")
-	awsSecretKey              string = "M_AWS_SECRET_KEY=" + os.Getenv("AWS_SECRET_KEY")
+	awsAccessKey              string = "M_AWS_ACCESS_KEY=" + os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretKey              string = "M_AWS_SECRET_KEY=" + os.Getenv("AWS_SECRET_ACCESS_KEY")
 	sharedFilePath                   = filepath.Join(path, "shared")
 	sharedAbsoluteFilePath, _        = filepath.Abs(sharedFilePath)
 	mountDir                         = sharedAbsoluteFilePath + ":/shared"
@@ -107,6 +113,66 @@ func TestOnApplyShouldCreateEnvironment(t *testing.T) {
 	// then
 	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
 	log.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
+}
+
+func TestShouldCheckNumberOfVms(t *testing.T) {
+	// given
+	reservationsNumber := 1
+	instancesNumber := 1
+
+	session, err := session.NewSession(&aws.Config{Region: aws.String("eu-central-1")})
+	if err != nil {
+		t.Fatal("Cannot get session.")
+	}
+
+	// when
+
+	// ec2
+	ec2Client := ec2.New(session)
+
+	filterParams := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name: aws.String("tag:Name"),
+				Values: []*string{
+					aws.String(awsTag),
+				},
+			},
+		},
+	}
+
+	ec2Result, err := ec2Client.DescribeInstances(filterParams)
+
+	// rg
+	rgClient := resourcegroups.New(session)
+
+	rgName := "rg-" + moduleName
+
+	rgResult, err := rgClient.GetGroup(&resourcegroups.GetGroupInput{
+		GroupName: aws.String(rgName),
+	})
+
+	log.Println("rg", rgResult.Group)
+
+	rgResourcesList, err := rgClient.ListGroupResources(&resourcegroups.ListGroupResourcesInput{
+		GroupName: aws.String(rgName),
+	})
+
+	log.Println(rgResourcesList)
+
+	// then
+	if err != nil {
+		t.Fatal("There was an error. ", err)
+	}
+
+	if len(ec2Result.Reservations) != 1 {
+		t.Error("Expected ", reservationsNumber, "reservation(s) got ", ec2Result.Reservations)
+	}
+
+	if len(ec2Result.Reservations[0].Instances) != 1 {
+		t.Error("Expected ", instancesNumber, "instance, got ", len(ec2Result.Reservations[0].Instances))
+	}
+
 }
 
 func TestOnDestroyPlanShouldDisplayDestroyPlan(t *testing.T) {
